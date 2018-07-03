d20673531f63aa2e785a08eb6437ae3de2cbd284
talithamedeiros <medeirostalitha@gmail.com>
2018-07-03 17:08:12 -0300

    requests to api

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app  import app
 from swapi import swapi
+import requests
  
 from app.models.forms import SearchForm
 
@@ -10,8 +11,23 @@ from app.models.forms import SearchForm
 def index():
     form = SearchForm()
     if form.validate_on_submit():
-        print(form.term.data)
-        print(form.type_search.data)
+        term = form.term.data
+        type_search = form.type_search.data
+        
+        if(type_search == 'people'):
+            r = requests.get('https://swapi.co/api/people/?search='+term)
+        elif(type_search == 'spaceships'):
+            r = requests.get('https://swapi.co/api/spaceships/?search='+term)    
+        elif(type_search == 'vehicles'):
+            r = requests.get('https://swapi.co/api/vehicles/?search='+term)    
+        elif(type_search == 'planets'):
+            r = requests.get('https://swapi.co/api/planets/?search='+term)    
+        elif(type_search == 'films'):
+            r = requests.get('https://swapi.co/api/films/?search='+term)    
+        else:
+            r = requests.get('https://swapi.co/api/species/?search='+term)    
+
+        print(r.text)
     else:
         print(form.errors)
     return render_template('index.html', form=form)

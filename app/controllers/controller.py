45986332e65e1113361eb2bfbcd0d43d7fa12bc6
talithamedeiros <medeirostalitha@gmail.com>
2018-07-03 17:52:11 -0300

    :rocket:

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app  import app
 from swapi import swapi
 import requests
+import json
+from jinja2 import Template
  
 from app.models.forms import SearchForm
 
@@ -13,21 +15,23 @@ def index():
     if form.validate_on_submit():
         term = form.term.data
         type_search = form.type_search.data
-        
-        if(type_search == 'people'):
-            r = requests.get('https://swapi.co/api/people/?search='+term)
-        elif(type_search == 'spaceships'):
-            r = requests.get('https://swapi.co/api/spaceships/?search='+term)    
-        elif(type_search == 'vehicles'):
-            r = requests.get('https://swapi.co/api/vehicles/?search='+term)    
-        elif(type_search == 'planets'):
-            r = requests.get('https://swapi.co/api/planets/?search='+term)    
-        elif(type_search == 'films'):
-            r = requests.get('https://swapi.co/api/films/?search='+term)    
-        else:
-            r = requests.get('https://swapi.co/api/species/?search='+term)    
 
-        print(r.text)
+        if(type_search == 'people'):
+            response = requests.get('https://swapi.co/api/people/?search='+term)
+        elif(type_search == 'spaceships'):
+            response = requests.get('https://swapi.co/api/spaceships/?search='+term)    
+        elif(type_search == 'vehicles'):
+            response = requests.get('https://swapi.co/api/vehicles/?search='+term)    
+        elif(type_search == 'planets'):
+            response = requests.get('https://swapi.co/api/planets/?search='+term)    
+        elif(type_search == 'films'):
+            response = requests.get('https://swapi.co/api/films/?search='+term)    
+        else:
+            response = requests.get('https://swapi.co/api/species/?search='+term)    
+
+        data = response.text
+        print(data)
+
     else:
         print(form.errors)
     return render_template('index.html', form=form)

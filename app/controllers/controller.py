b2f88f530a9c80069446368f9e134cb20c5eb9d2
talithamedeiros <medeirostalitha@gmail.com>
2018-07-03 14:13:42 -0300

    Add form

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,11 +1,18 @@
 from flask import render_template
 from app  import app
-
+from swapi import swapi
+ 
 from app.models.forms import SearchForm
 
 
-@app.route("/index")
-@app.route("/")
+@app.route("/index", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = SearchForm()
+    if form.validate_on_submit():
+        print(form.term.data)
+        print(form.type_search.data)
+        print(swapi.get_person(1))
+    else:
+        print(form.errors)
     return render_template('index.html', form=form)
--- app/models/forms.py
+++ app/models/forms.py
@@ -1,14 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField
+from wtforms import StringField
+from wtforms import RadioField
+from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    term = StringField("term")
-    type_search = RadioField('type_search', 
+    term = StringField(u'Term', validators=[DataRequired()])
+    type_search = RadioField(u'Types',
+                    validators=[DataRequired()], 
                     choices = [('planets', 'Planets'),
                     ('spaceships', 'Spaceships'),
                     ('vehicles', 'Vehicles'),
                     ('people', 'People'),
-                    ('films', 'films'),
-                    ('species', 'Species')])
+                    ('films', 'Films'),
+                    ('species', 'Species')], 
+                    default='planets')
      
